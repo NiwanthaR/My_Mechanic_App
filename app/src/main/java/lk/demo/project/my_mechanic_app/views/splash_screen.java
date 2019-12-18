0b6e05766cc071eb0d39d5d55f15aeb657af12e4
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2019-12-18 11:34:17 +0530

    Start
    Create Splash Screen
    MVC Structure

---
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/splash_screen.java
@@ -0,0 +1,28 @@
+package lk.demo.project.my_mechanic_app.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import lk.demo.project.my_mechanic_app.MainActivity;
+import lk.demo.project.my_mechanic_app.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash_screen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.TYPE_INPUT_METHOD_DIALOG);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
