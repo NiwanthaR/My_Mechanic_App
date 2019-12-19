cfe60e5c2474bfe83ac847d0a674e6942cb98c97
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2019-12-19 13:05:30 +0530

    complete client signup half

---
+++ app/src/main/java/lk/demo/project/my_mechanic_app/control/validation_client_signup.java
@@ -0,0 +1,88 @@
+package lk.demo.project.my_mechanic_app.control;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validation_client_signup {
+
+    public static boolean is_fill(String email,String password)
+    {
+        if (email.isEmpty() || password.isEmpty())
+            return false;
+        else
+            return true;
+    }
+
+    public static boolean is_password_same(String password,String repassword)
+    {
+        if (password.equals(repassword))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean is_fill(String fname,String lname,String nic,String dob,String address,String city,String contact,String email,String password)
+    {
+        if (fname.isEmpty() || lname.isEmpty() || nic.isEmpty() || dob.isEmpty() || address.isEmpty() || city.isEmpty() || contact.isEmpty() || email.isEmpty() || password.isEmpty())
+            return false;
+        else
+            return true;
+    }
+
+    public static boolean is_ValidNic(String nic) {
+
+        String stringToSearch = nic;
+
+        Pattern p = Pattern.compile("([0-9]{9}[a-z]{1})");
+        Matcher m = p.matcher(stringToSearch);
+
+
+        if (m.find() && nic.length()==10)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean is_ValidPassword(String password) {
+
+        String stringToSearch = password;
+
+        Pattern p = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])");
+        Matcher m = p.matcher(stringToSearch);
+
+
+        if (m.find() && password.length()>7)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static boolean is_Validmail(final String email)
+    {
+        String StringTosearch = email;
+
+        Pattern p = Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$");
+        Matcher m = p.matcher(StringTosearch);
+
+
+        if (m.find())
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean is_contact(final String contact)
+    {
+        String StringTosearch = contact;
+
+        Pattern p = Pattern.compile("(^1300\\d{6}$)|(^0[1|3|7|6|8]{1}[0-9]{8}$)|(^13\\d{4}$)|(^04\\d{2,3}\\d{6}$)");
+        Matcher m = p.matcher(StringTosearch);
+
+
+        if (m.find())
+            return true;
+        else
+            return false;
+    }
+}
--- app/src/main/java/lk/demo/project/my_mechanic_app/views/client_signup_dash.java
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/client_signup_dash.java
@@ -2,17 +2,183 @@ package lk.demo.project.my_mechanic_app.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import lk.demo.project.my_mechanic_app.R;
+import lk.demo.project.my_mechanic_app.control.validation_client_signup;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class client_signup_dash extends AppCompatActivity {
 
+    private EditText user_fname,user_lname,user_nic,user_dob,user_gender,user_address,user_city,user_contact,user_email,user_password,user_repassword;
+    private Button register_btn,goback_btn;
+    private CheckBox show_password_cb;
+    private TextView worng_details;
 
+    private String fname,lname,nic,dob,gender,address,city,contact,email,password,repassword;
+
+    //datepicker
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
+
+    //gender read
+    private RadioGroup gender_group;
+    private RadioButton select_gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_signup_dash);
+
+        //assign variable
+        Assign_variable();
+
+        //press go back button code
+        goback_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(client_signup_dash.this,client_login_dash.class));
+            }
+        });
+
+        //date of birth select dialog
+        user_dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar= Calendar.getInstance();
+                int date=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
+                int year=calendar.get(Calendar.YEAR);
+                datePickerDialog=new DatePickerDialog(client_signup_dash.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        user_dob.setText(year +"/"+ (month+1) +"/"+ dayOfMonth);
+                    }
+                },year,month,date);
+                datePickerDialog.show();
+            }
+        });
+
+        show_password_cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    user_password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    user_repassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }else{
+                    user_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    user_repassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fname=user_fname.getText().toString();
+                lname=user_lname.getText().toString();
+                nic=user_nic.getText().toString();
+                dob=user_dob.getText().toString();
+                //read gender
+                read_gender();
+
+                address=user_address.getText().toString();
+                city=user_city.getText().toString();
+                contact=user_contact.getText().toString();
+                email=user_email.getText().toString();
+                password=user_password.getText().toString();
+                repassword=user_repassword.getText().toString();
+
+                if (all_right())
+                {
+                    Toast.makeText(client_signup_dash.this,"Still Correct",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    private void read_gender()
+    {
+        // get selected radio button from radioGroup
+        int selectedId = gender_group.getCheckedRadioButtonId();
+
+        // find the radiobutton by returned id
+        select_gender = (RadioButton) findViewById(selectedId);
+
+        gender= String.valueOf(select_gender.getText());
+        //Toast.makeText(Post_Add_Home_Dash.this,h_kitchen,Toast.LENGTH_SHORT).show();
+    }
+
+    void Assign_variable()
+    {
+        user_fname=(EditText)findViewById(R.id.et_fname_client_signup);
+        user_lname=(EditText)findViewById(R.id.et_lname_client_signup);
+        user_nic=(EditText)findViewById(R.id.et_nic_client_signup);
+        user_dob=(EditText)findViewById(R.id.et_dob_client_signup);
+        user_address=(EditText)findViewById(R.id.et_address_client_signup);
+        user_city=(EditText)findViewById(R.id.et_city_client_signup);
+        user_contact=(EditText)findViewById(R.id.et_contact_client_signup);
+        user_email=(EditText)findViewById(R.id.et_email_client_signup);
+        user_password=(EditText)findViewById(R.id.et_password_client_signup);
+        user_repassword=(EditText)findViewById(R.id.et_repassword_client_signup);
+
+        register_btn=(Button)findViewById(R.id.btn_registernow_client_signup);
+        goback_btn=(Button)findViewById(R.id.btn_alredysign_client_signup);
+        show_password_cb=(CheckBox)findViewById(R.id.cb_show_password_client_signup);
+
+        worng_details=(TextView)findViewById(R.id.tv_wrong_client_signup);
+
+        gender_group=(RadioGroup)findViewById(R.id.radio_gender_client_signup);
+    }
+
+    private boolean all_right()
+    {
+        if (validation_client_signup.is_fill(fname,lname,nic,dob,address,city,contact,email,password))
+        {
+            if (validation_client_signup.is_password_same(password,repassword))
+            {
+                if (validation_client_signup.is_ValidPassword(password))
+                {
+                    if (validation_client_signup.is_Validmail(email))
+                    {
+                        if (validation_client_signup.is_ValidNic(nic))
+                        {
+                            if (validation_client_signup.is_contact(contact)){
+                                return true;
+                            }else{
+                                worng_details.setText("Please Enter Valid Contact");
+                            }
+                        }else {
+                            worng_details.setText("Please Enter Valid NIC");
+                        }
+                    }else {
+                        worng_details.setText("Your Email is not Valid");
+                    }
+                }else {
+                    worng_details.setText("Your Password not Valid");
+                }
+            }else {
+                worng_details.setText("Your Password isn't Same..!!");
+            }
+        } else {
+            worng_details.setText("Please Fill All Details...!!");
+        }
+        return false;
     }
 }
