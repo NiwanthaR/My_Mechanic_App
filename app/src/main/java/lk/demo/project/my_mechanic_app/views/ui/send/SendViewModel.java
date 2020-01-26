857865989482ba72c94cfab6fb5f3d6147597a1b
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-01-26 20:47:15 +0530

    Add New client Dashboard

---
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/ui/send/SendViewModel.java
@@ -0,0 +1,19 @@
+package lk.demo.project.my_mechanic_app.views.ui.send;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
