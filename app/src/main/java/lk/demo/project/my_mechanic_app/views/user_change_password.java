cd8025abef855443cd3428690303c3fb0ed267f2
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-02-11 02:38:56 +0530

    change password update

--- app/src/main/java/lk/demo/project/my_mechanic_app/views/user_change_password.java
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/user_change_password.java
@@ -7,8 +7,11 @@ import lk.demo.project.my_mechanic_app.control.validation_client_signup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -25,10 +28,10 @@ public class user_change_password extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
 
-    LinearLayout pass_conferm,pass_update;
+    private LinearLayout pass_conferm,pass_update;
     private Button re_Auth_pass,update_pass_submit;
     private EditText pass_re_Auth,pass_new_pass,pass_new_re_pass;
-
+    private CheckBox auth_pass_show,new_pass_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,6 +101,32 @@ public class user_change_password extends AppCompatActivity {
             }
         });
 
+        auth_pass_show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    pass_re_Auth.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }else{
+                    pass_re_Auth.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+
+        new_pass_show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    pass_new_pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    pass_new_re_pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }else{
+                    pass_new_pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    pass_new_re_pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+
     }
 
     private void Assing_variable()
@@ -114,5 +143,8 @@ public class user_change_password extends AppCompatActivity {
 
         firebaseAuth=FirebaseAuth.getInstance();
         firebaseUser=firebaseAuth.getCurrentUser();
+
+        auth_pass_show=findViewById(R.id.cb_show_password_client_cp1);
+        new_pass_show=findViewById(R.id.cb_show_password_client_cp2);
     }
 }
