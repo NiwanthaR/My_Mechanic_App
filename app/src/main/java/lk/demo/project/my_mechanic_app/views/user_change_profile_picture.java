324764eeea62660d6dde1f353710f38687809249
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-02-28 22:39:25 +0530

    user change profile picture complete

--- app/src/main/java/lk/demo/project/my_mechanic_app/views/user_change_profile_picture.java
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/user_change_profile_picture.java
@@ -1,15 +1,148 @@
 package lk.demo.project.my_mechanic_app.views;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import lk.demo.project.my_mechanic_app.R;
+import lk.demo.project.my_mechanic_app.control.validation_client_signup;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class user_change_profile_picture extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private FirebaseUser firebaseUser;
+
+    //Button
+    private Button upload_image;
+
+    //Imageview
+    private ImageView profile_image,new_profile_image;
+
+    //Image upload part
+    private static int PICK_IMG=123;
+    private Uri image_path;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_change_profile_picture);
+
+        //load ui value
+        Assign_variable();
+
+        //load current profile image
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child("Profile Picture").child(firebaseAuth.getUid()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().centerCrop().into(profile_image);
+            }
+        });
+
+        //load new image
+        new_profile_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Profile Picture"),PICK_IMG);
+            }
+        });
+
+        upload_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (validation_client_signup.is_image_ok(image_path))
+                {
+                    upload_image_new();
+                }else{
+                    Toast.makeText(user_change_profile_picture.this,"Please Select Image",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void Assign_variable() {
+
+        //firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        storageReference = firebaseStorage.getReference();
+
+        //image upload
+        upload_image = findViewById(R.id.btn_user_change_image);
+
+        //image profile
+        profile_image = findViewById(R.id.img_user_change_image);
+        new_profile_image = findViewById(R.id.img_user_upload_new_image);
+    }
+
+    //get new profile picture
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_IMG && resultCode == RESULT_OK && data.getData() != null)
+        {
+            image_path = data.getData();
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),image_path);
+                new_profile_image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void upload_image_new()
+    {
+        StorageReference myReference = storageReference.child("Profile Picture").child(firebaseAuth.getUid());
+        UploadTask uploadTask = myReference.putFile(image_path);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(user_change_profile_picture.this,"Image Upload Failed",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(user_change_profile_picture.this,"Image Upload Successfully",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
