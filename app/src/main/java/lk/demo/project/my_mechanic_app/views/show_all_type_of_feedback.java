c42421fce3ba924adc77b9593cabd415b75f7ae1
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-03-13 03:07:36 +0530

    Update show_all_type_of_feedback.java

--- app/src/main/java/lk/demo/project/my_mechanic_app/views/show_all_type_of_feedback.java
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/show_all_type_of_feedback.java
@@ -21,6 +21,7 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 public class show_all_type_of_feedback extends AppCompatActivity {
@@ -39,6 +40,9 @@ public class show_all_type_of_feedback extends AppCompatActivity {
     //Seller ID
     private String Seller_Key,Seller_Store,User_Name;
 
+    //Quary
+    private Query all_feedback,positive_feedback,negative_feedback,neutral_feedback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,8 +59,11 @@ public class show_all_type_of_feedback extends AppCompatActivity {
         //start view
         all_feed.setBackgroundColor(Color.parseColor("#95EAFF"));
 
+        //loard all feedback quary
+        all_feedback = DataRef.child(Seller_Key).orderByChild("User_Satisfaction").startAt("").endAt(""+"\uf8ff");
+
         //Load Recycler Data
-        Load_Data();
+        Load_Data(all_feedback);
 
 
         //click all go add feedback
@@ -82,6 +89,9 @@ public class show_all_type_of_feedback extends AppCompatActivity {
                 positive_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 neutrel_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 negative_fee.setBackgroundColor(Color.parseColor("#FBFCFC"));
+
+                //loard feedback
+                Load_Data(all_feedback);
             }
         });
 
@@ -94,6 +104,13 @@ public class show_all_type_of_feedback extends AppCompatActivity {
                 neutrel_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 negative_fee.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 all_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
+
+                //load positive feedback
+                String V1 = "Positive";
+                positive_feedback = DataRef.child(Seller_Key).orderByChild("User_Satisfaction").startAt(V1).endAt(V1+"\uf8ff");
+
+                //loard feedback
+                Load_Data(positive_feedback);
             }
         });
 
@@ -106,6 +123,13 @@ public class show_all_type_of_feedback extends AppCompatActivity {
                 positive_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 negative_fee.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 all_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
+
+                //loard neutral feedback
+                String V2 = "Neutral";
+                neutral_feedback = DataRef.child(Seller_Key).orderByChild("User_Satisfaction").startAt(V2).endAt(V2+"\uf8ff");
+
+                //loard feedback
+                Load_Data(neutral_feedback);
             }
         });
 
@@ -118,6 +142,13 @@ public class show_all_type_of_feedback extends AppCompatActivity {
                 neutrel_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 positive_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
                 all_feed.setBackgroundColor(Color.parseColor("#FBFCFC"));
+
+                //loard negative feedback
+                String V3 = "Negative";
+                negative_feedback = DataRef.child(Seller_Key).orderByChild("User_Satisfaction").startAt(V3).endAt(V3+"\uf8ff");
+
+                //loard feedback
+                Load_Data(negative_feedback);
             }
         });
     }
@@ -143,9 +174,9 @@ public class show_all_type_of_feedback extends AppCompatActivity {
 
     }
 
-    private void Load_Data()
+    private void Load_Data(Query query)
     {
-        options = new FirebaseRecyclerOptions.Builder<user_feedback>().setQuery(DataRef.child(Seller_Key),user_feedback.class).build();
+        options = new FirebaseRecyclerOptions.Builder<user_feedback>().setQuery(query,user_feedback.class).build();
 
         adapter = new FirebaseRecyclerAdapter<user_feedback, MyFeedbackHolder>(options) {
             @Override
