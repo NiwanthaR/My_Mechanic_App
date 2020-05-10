ca0403a0b6fef95202f997bbfeba049d31a657a4
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-05-10 09:55:30 +0530

    Backend Fix 02

--- app/src/main/java/lk/demo/project/my_mechanic_app/views/mechanic_post_view_dash_edit.java
+++ app/src/main/java/lk/demo/project/my_mechanic_app/views/mechanic_post_view_dash_edit.java
@@ -5,15 +5,26 @@ import lk.demo.project.my_mechanic_app.R;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class mechanic_post_view_dash_edit extends AppCompatActivity {
 
     //key Declare
     private String AD_Number;
-
     //Layout
     private LinearLayout state_layout,buttons_layout;
+    //Edittext
+    private EditText post_title,post_description,post_price,post_contact;
+    //Textview
+    private TextView post_condition,post_store,post_store_location,post_owner,wrong_details,presantage;
+    //Progressbar
+    private ProgressBar upload_state;
+    //Buttons
+    private Button btn_update,btn_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +44,26 @@ public class mechanic_post_view_dash_edit extends AppCompatActivity {
         buttons_layout = findViewById(R.id.li_mechanic_post_details_edit_Buttons);
 
         state_layout.setVisibility(View.GONE);
+
+        //Buttons
+        btn_delete = findViewById(R.id.btn_mechanic_post_details_edit_delete_add);
+        btn_update = findViewById(R.id.btn_mechanic_post_details_edit_edit_add);
+
+        //Textview
+        post_condition = findViewById(R.id.tv_mechanic_post_details_edit_condition);
+        post_store = findViewById(R.id.tv_mechanic_post_details_edit_store_name);
+        post_store_location = findViewById(R.id.tv_mechanic_post_details_edit_store_location);
+        post_owner = findViewById(R.id.tv_mechanic_post_details_edit_owner_name);
+        wrong_details = findViewById(R.id.tv_mechanic_post_details_edit_wrong_details);
+        presantage = findViewById(R.id.et_mechanic_post_details_edit_progress_presantage);
+
+        //Editetext
+        post_title = findViewById(R.id.et_mechanic_post_details_edit_title);
+        post_description = findViewById(R.id.et_mechanic_post_details_edit__description);
+        post_price = findViewById(R.id.et_mechanic_post_details_edit_price);
+        post_contact = findViewById(R.id.tv_mechanic_post_details_edit_contact_number);
+
+        //Progressbar
+        upload_state = findViewById(R.id.et_mechanic_post_details_edit_upload_progess_bar);
     }
 }
